1b9ece27c607604da2f7bc31a6eeb3551f153816
onetarek <onetarek@gmail.com>
2022-08-25 22:12:14 +0600

    change version

--- filter-page-by-template.php
+++ filter-page-by-template.php
@@ -5,7 +5,7 @@ Description: See list of pages or any type of posts by filtering based on used t
 Plugin URI: http://onetarek.com/my-wordpress-plugins/filter-page-by-template
 Author: oneTarek
 Author URI: http://onetarek.com
-Version: 2.2
+Version: 3.0
 */
 
 define ( 'FILTER_PAGE_BY_TEMPLATE_PLUGIN_DIR', dirname(__FILE__) ); // Plugin Directory
