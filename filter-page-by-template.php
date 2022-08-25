6da10099d2f49e75b738c974404dd908ee248a59
onetarek <onetarek@gmail.com>
2022-08-25 06:41:33 +0600

    Update code style

--- filter-page-by-template.php
+++ filter-page-by-template.php
@@ -13,20 +13,18 @@ define ( 'FILTER_PAGE_BY_TEMPLATE_PLUGIN_URL', plugin_dir_url(__FILE__) ); // Pl
 
 class Filter_Page_By_Template {
 	
-	public function __construct()
-	{
+	public function __construct() {
 		add_action( 'restrict_manage_posts', array( $this, 'filter_dropdown' ) );
 		add_filter( 'request', array( $this, 'filter_post_list' ) );
 		
-		add_filter('manage_pages_columns', array( $this, 'post_list_columns_head'));
-		add_action('manage_pages_custom_column', array( $this, 'post_list_columns_content' ), 10, 2);
+		add_filter('manage_pages_columns', array( $this, 'post_list_columns_head') );
+		add_action('manage_pages_custom_column', array( $this, 'post_list_columns_content' ), 10, 2 );
 
 		add_action( 'init', array( $this , 'load_textdomain' ) );
 
 	}
 	
-	public function filter_dropdown()
-	{
+	public function filter_dropdown() {
 		if ( $GLOBALS['pagenow'] === 'upload.php' ) {
 			return;
 		}
@@ -43,23 +41,22 @@ class Filter_Page_By_Template {
 		<?php	
 	}//end func 
 	
-	public function filter_post_list( $vars ){
+	public function filter_post_list( $vars ) {
 
 		if ( ! isset( $_GET['page_template_filter'] ) ) return $vars;
-		$template = trim($_GET['page_template_filter']);
+		$template = trim( $_GET['page_template_filter'] );
 
-		$data = get_option("filter_page_by_template_data", array() );
+		$data = get_option( "filter_page_by_template_data", array() );
 		$filter_used = isset( $data['filter_used'] ) ? intval( $data['filter_used'] ) : 0;
 		$filter_used ++;
 	    $data['filter_used'] = $filter_used;
-	    update_option("filter_page_by_template_data", $data );
+	    update_option( "filter_page_by_template_data", $data );
 
 		if ( $template == "" || $template == 'all' ) return $vars;
 		
-		if( $template == 'all_missing')
-		{
+		if( $template == 'all_missing') {
 			$templates = wp_get_theme()->get_page_templates( null, 'page' );
-			$template_files = array_keys($templates);
+			$template_files = array_keys( $templates );
 			$template_files[] = 'default';
 			$vars = array_merge(
 				$vars,
@@ -73,9 +70,7 @@ class Filter_Page_By_Template {
 					),
 				)
 			);
-		}
-		else
-		{
+		} else {
 			$vars = array_merge(
 				$vars,
 				array(
@@ -95,37 +90,27 @@ class Filter_Page_By_Template {
 	}//end func
 
 	# Add new column to post list
-	public function post_list_columns_head( $columns )
-	{
+	public function post_list_columns_head( $columns ) {
 	    $columns['template'] = 'Template';
 	    return $columns;
 	}
 	 
 	#post list column content
-	public function post_list_columns_content( $column_name, $post_id )
-	{
+	public function post_list_columns_content( $column_name, $post_id ) {
 	    $post_type = 'page';
 
-	    if($column_name == 'template')
-	    {
-	        $template = get_post_meta($post_id, "_wp_page_template" , true );
-	        if($template)
-	        {
-	        	if($template == 'default')
-	        	{
+	    if( $column_name == 'template' ) {
+	        $template = get_post_meta( $post_id, "_wp_page_template" , true );
+	        if( $template ) {
+	        	if( $template == 'default' ) {
 	        		$template_name = apply_filters( 'default_page_template_title',  __( 'Default Template' ), 'meta-box' );
 	        		echo '<span title="' . esc_attr( __( 'Template file', 'filter-page-by-template' ) ) . ': page.php">'.$template_name.'</span>';
-	        	}
-	        	else
-	        	{
+	        	} else {
 	        		$templates = wp_get_theme()->get_page_templates( null, $post_type );
 
-	        		if( isset( $templates[ $template ] ) )
-	        		{
+	        		if( isset( $templates[ $template ] ) ) {
 	        			echo '<span title="Template file: '.$template.'">'.$templates[ $template ].'</span>';
-	        		}
-	        		else
-	        		{
+	        		} else {
 	        			
 	        			echo '<span style="color:red" title="' . esc_attr( __( 'This template file does not exist', 'filter-page-by-template' ) ) . '">'.$template.'</span>';
 	        		}
@@ -139,15 +124,14 @@ class Filter_Page_By_Template {
 	 * @since 2.0
 	 * @return void
 	 */
-	public function load_textdomain(){
+	public function load_textdomain() {
 		load_plugin_textdomain( 'filter-page-by-template', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 	
 	
 }//end class
 
-if( is_admin() )
-{
+if( is_admin() ) {
    new Filter_Page_By_Template();
    include_once(FILTER_PAGE_BY_TEMPLATE_PLUGIN_DIR."/includes/five_star_wp_rate_notice.php"); 
 }
